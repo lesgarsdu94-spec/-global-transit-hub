44ef740a9c7bc30b5eb1b1abcef5deb3c88bdce5
DoggyCat25 <lesgarsdu94@gmail.com>
2026-07-04 23:22:36 +0200

    Create translations.js

---
+++ SRC/data/translations.js
@@ -0,0 +1,7 @@
+export const locales = {
+  fr: { code: "🇫🇷 FR", mode: "Contributeur", real: "Réel", fake: "Fictif", color: "Couleur", place: "Dépôt" },
+  es: { code: "🇪🇸 ES", mode: "Colaborador", real: "Real", fake: "Ficticio", color: "Color", place: "Depósito" },
+  de: { code: "🇩🇪 DE", mode: "Mitwirkender", real: "Real", fake: "Fiktiv", color: "Farbe", place: "Depot" },
+  gb: { code: "🇬🇧 UK", mode: "Contributor", real: "Real", fake: "Fictional", color: "Colour", place: "Depot" },
+  us: { code: "🇺🇸 US", mode: "Contributor", real: "Real", fake: "Fictional", color: "Color", place: "Garage" }
+};
